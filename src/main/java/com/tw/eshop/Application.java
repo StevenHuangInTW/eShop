a431be5961374337b64b23c92b1e14ea3a36408d
Steven Huang <qbhuang@thoughtworks.com>
2016-08-18 16:54:11 +0800

    Add servlet initialization.

--- src/main/java/com/tw/eshop/Application.java
+++ src/main/java/com/tw/eshop/Application.java
@@ -23,6 +23,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
@@ -31,7 +33,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EntityScan(basePackages = {"com.tw.eshop.model"})
 @EnableJpaRepositories(basePackages = {"com.tw.eshop.dao"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	@Autowired
 	private OrderService orderService;
