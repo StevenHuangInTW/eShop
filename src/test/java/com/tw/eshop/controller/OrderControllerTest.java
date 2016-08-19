683623318b8a732bec9a2b32b9821663596087ee
Steven Huang <qbhuang@thoughtworks.com>
2016-08-19 13:45:07 +0800

    Refactor the code by interface.

---
+++ src/test/java/com/tw/eshop/controller/OrderControllerTest.java
@@ -0,0 +1,64 @@
+package com.tw.eshop.controller;
+
+import com.tw.eshop.service.OrderService;
+import com.tw.eshop.validator.ValidationBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by qbhuang on 16/8/19.
+ */
+
+@WebMvcTest(OrderController.class)
+@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+public class OrderControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private ValidationBuilder validationBuilder;
+
+    @Test
+    public void givenOrderInformationShouldReturnOrder() throws Exception {
+       // given(this.orderService.getVehicleDetails("sboot"))
+         //       .willReturn(new VehicleDetails("Honda", "Civic"));
+
+        this.mvc.perform(put("order").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+//                .andExpect(content().json("{'name':'test_order','price':2}"));
+    }
+}
